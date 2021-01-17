e799fc25aff6ea3898c2df28e16c5df4bedf83d2
carinak6 <kassiscarina@gmail.com>
2021-01-17 14:23:27 +0100

    modification et commentaires

--- pendu.py
+++ pendu.py
@@ -6,14 +6,17 @@ import json
 import requests as req
 import urllib.request
 
+#bloque de configuration du fichier log
 logging.basicConfig(filename='logs.log',level=logging.DEBUG,\
       format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
 
 logging.info("Verification overture de fichier mots. START")
+#on verifie si le site envoie bien les mots
 if urllib.request.urlopen("http://www.mit.edu/~ecprice/wordlist.10000").getcode() != 200:
+      #s'il y a en erreur on affiche un fichier en local
       f= open("./assets/mots.txt","r")
       backup_liste = f.read()
-      reponse2 = backup_liste.split(',')
+      reponse2 = backup_liste.split(',') #on cree un liste des mots
 else:      
       resp = req.get("http://www.mit.edu/~ecprice/wordlist.10000")
       logging.info("Verification overture de fichier mots. FIN")
@@ -22,6 +25,7 @@ else:
 
       #transformer le type byte de content en string
       reponse = mots.decode('utf-8')
+
       #on cree un liste des mots
       reponse2 = reponse.split("\n")
 
@@ -29,6 +33,7 @@ else:
 
 life = 7
 
+#liste avec le code ascii du pendu
 HANGMANPICS = ['''
   +---+
   |   |
@@ -80,48 +85,56 @@ HANGMANPICS = ['''
       |
 =========''']
 
-
+#on choisi un mot al hazar
 chosen_word = random.choice(reponse2) 
-print(chosen_word)
-chaineDecouvrir=[]
+
+print(chosen_word) #pour verifier
+
+chaineDecouvrir=[] #variable de type liste avec les lettres que on aura decouverte
+
+#boucle pour asigner les tires qui correspondant au mots choisie
 for i in range(len(chosen_word)) :
       chaineDecouvrir.append('-')
       #print('-', end=' ')
 
 print(chaineDecouvrir)
 
-nroPendu=0
-drapeu = True
-listeLettresChoisi=set()
+nroPendu=0 #variable qui affichera la partie du pendu
+finJeux = False #variable pour sortir de la boucle
+listeLettresChoisi=set() #variable pour afficher les lettres dejà choisie
 
-while drapeu:
+while finJeux == False:
       if len(listeLettresChoisi) != 0:
             print("Lettres deja choisi : ", ' '.join(listeLettresChoisi))
+
       guest = input("\nChoisir une lettre : ")
-      print(guest)
-      listeLettresChoisi.add(guest)
-      if guest in chosen_word:
+      print(guest) #on affiche la letre
+
+      listeLettresChoisi.add(guest) #ajoute la lettre dans le set
+
+      if guest in chosen_word: #on demande si la lettre est partie du mot choisie
             #lprint('trouvé')
-            for ki,x in enumerate(chosen_word):            
+            for ki,x in enumerate(chosen_word):#on ajoute la lettre dans la liste avec le mot a decouvrir
                   if guest == x :
                         chaineDecouvrir[ki]=guest                  
             
-      else:
+      else: #le lettre n'est pas partie du mot à decouvrir
             life -=1
             print('il vous reste :',life,' vies')
             print(HANGMANPICS[nroPendu])
-            nroPendu+=1
+            nroPendu+=1 #variable pour savoir quel parti de l'ascii on affiche la prochaine fois
             
-      print(chaineDecouvrir)
-      motsDecouvrir = ''.join(chaineDecouvrir)
-     #print('mot en decouverte :', motsDecouvrir )
-      if motsDecouvrir == chosen_word :
+      print(chaineDecouvrir) #liste de lettre de mots à decouvrir
+      motsDecouvrir = ''.join(chaineDecouvrir) #transforme la liste en string, pour la comparer avec le mots choisie
+     #print('mot decouverte :', motsDecouvrir )
+
+      if motsDecouvrir == chosen_word : #on verifie si le mots decouverte est egal à la mots choisie
             print('Vous avez Gangné!!!')
-            drapeu=False #pour sortir du boucle
-      elif life == 0:
+            finJeux=True #pour sortir du boucle
+      elif life == 0: # verifie si il n y a pas de vie
             print('pendu')
-            drapeu=False #pour sortir du boucle
-      else:
+            finJeux=True #pour sortir du boucle
+      else:#demande si on veut essayer de diviner le mot
             print("Vous voulez essaie le mot à diviner [Oui] / [Non]?")
             res = input()
             if res == 'o':
@@ -129,7 +142,7 @@ while drapeu:
                   respuesta = input()
                   if respuesta == chosen_word:
                         print('Vous avez Gangné!!!')
-                        drapeu=False #pour sortir du boucle
+                        finJeux=True #pour sortir du boucle
                         
 
 
